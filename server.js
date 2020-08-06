8e6c1089cf527e4ceac491d1a183daa783e63e45
Victor Cesar Lopez <civ187@gmail.com>
2020-08-06 16:22:00 -0500

    12.2.6 08/06/2020 @ 1621

--- server.js
+++ server.js
@@ -16,7 +16,95 @@ const db = new sqlite3.Database('./db/election.db', err => {
     console.log('Connected to the election database.');
   });
 
+// Create a candidate
+// const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected) 
+//               VALUES (?,?,?,?)`;
+// const params = [1, 'Ronald', 'Firbank', 1];
+// // ES5 function, not arrow function, to use this
+// db.run(sql, params, function(err, result) {
+//   if (err) {
+//     console.log(err);
+//   }
+//   console.log(result, this.lastID);
+// });
 
+// Delete a candidate
+// db.run(`DELETE FROM candidates WHERE id = ?`, 1, function(err, result) {
+//     if (err) {
+//       console.log(err);
+//     }
+//     console.log(result, this, this.changes);
+//   });
+
+// Delete a candidate
+app.delete('/api/candidate/:id', (req, res) => {
+    const sql = `DELETE FROM candidates WHERE id = ?`;
+    const params = [req.params.id];
+    db.run(sql, params, function(err, result) {
+      if (err) {
+        res.status(400).json({ error: res.message });
+        return;
+      }
+  
+      res.json({
+        message: 'successfully deleted',
+        changes: this.changes
+      });
+    });
+  });
+
+
+// Get all candidates --- API endpoint to retrieve all the candidates from the candidates table
+app.get('/api/candidates', (req, res) => {
+    const sql = `SELECT * FROM candidates`;
+    const params = [];
+    db.all(sql, params, (err, rows) => {
+      if (err) {
+        res.status(500).json({ error: err.message });
+        return;
+      }
+  
+      res.json({
+        message: 'success',
+        data: rows
+      });
+    });
+  });
+
+// Get single candidate --- API endpoint to get a single candidate
+app.get('/api/candidate/:id', (req, res) => {
+    const sql = `SELECT * FROM candidates 
+                 WHERE id = ?`;
+    const params = [req.params.id];
+    db.get(sql, params, (err, row) => {
+      if (err) {
+        res.status(400).json({ error: err.message });
+        return;
+      }
+  
+      res.json({
+        message: 'success',
+        data: row
+      });
+    });
+  });
+
+
+
+
+
+// GET a single candidate
+// db.get(`SELECT * FROM candidates WHERE id = 1`, (err, row) => {
+//     if(err) {
+//       console.log(err);
+//     }
+//     console.log(row);
+//   });
+
+//   return all the data in the candidates table
+// db.all(`SELECT * FROM candidates`, (err, rows) => {
+//     console.log(rows);
+//   });
 
 // app.get('/', (req, res) => {
 //     res.json({
@@ -28,11 +116,6 @@ const db = new sqlite3.Database('./db/election.db', err => {
 app.use((req, res) => {
     res.status(404).end();
   });
-
-
-
-
-
 
 // function that will start the Express.js server on port 3001
 // Start server after DB connection
