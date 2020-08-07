b6139dbf32e363608ccd44523d01c9dd7d1b2c0e
Victor Cesar Lopez <civ187@gmail.com>
2020-08-07 14:25:07 -0500

    12.4.6 08/07/20 @ 1425

---
+++ routes/apiRoutes/partyRoutes.js
@@ -0,0 +1,52 @@
+const express = require('express');
+const router = express.Router();
+const db = require('../../db/database');
+
+router.delete('/party/:id', (req, res) => {
+    const sql = `DELETE FROM parties WHERE id = ?`;
+    const params = [req.params.id];
+    db.run(sql, params, function(err, result) {
+      if (err) {
+        res.status(400).json({ error: res.message });
+        return;
+      }
+  
+      res.json({ message: 'successfully deleted', changes: this.changes });
+    });
+  });
+
+// Get all parties --- API endpoint to retrieve all the parties from the parties table
+router.get('/parties', (req, res) => {
+    const sql = `SELECT * FROM parties`;
+    const params = [];
+    db.all(sql, params, (err, rows) => {
+      if (err) {
+        res.status(500).json({ error: err.message });
+        return;
+      }
+  
+      res.json({
+        message: 'success',
+        data: rows
+      });
+    });
+  });
+  
+  // Get single party --- API endpoint to retrieve a single party from the parties table
+  router.get('/party/:id', (req, res) => {
+    const sql = `SELECT * FROM parties WHERE id = ?`;
+    const params = [req.params.id];
+    db.get(sql, params, (err, row) => {
+      if (err) {
+        res.status(400).json({ error: err.message });
+        return;
+      }
+  
+      res.json({
+        message: 'success',
+        data: row
+      });
+    });
+  });
+
+module.exports = router;
